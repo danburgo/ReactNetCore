dd4d00f2f8f34ba9a91a2d04fa74a51863fbd85e
Dan Burdeinick <danbur19@hotmail.com>
2018-10-24 23:03:23 -0400

    Finished Adding Session middleware and interaction with the webapp

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import {speakers} from './speakers';
-
+import {sessions} from './sessions';
 
 export default combineReducers({
-    speakers
+    speakers,
+    sessions
 });
